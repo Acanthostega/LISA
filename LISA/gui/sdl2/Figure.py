f28e002f2d7051cf5c71948aa47539c1989fd839
Acanthostega <acanthostega@hotmail.fr>
2014-08-14 17:11:01 +0200

    Add thread to manage event loop

--- LISA/gui/sdl2/Figure.py
+++ LISA/gui/sdl2/Figure.py
@@ -2,6 +2,7 @@
 
 from OpenGL.arrays import numpymodule
 from .events import SDLInput
+from threading import Thread, Lock
 
 import sdl2 as s
 
@@ -9,8 +10,8 @@ import sdl2 as s
 numpymodule.NumpyHandler.ERROR_ON_COPY = True
 
 
-class SDL2_Dealer(object):
-    def __init__(self, fps=60):
+class SDL2_Deal(object):
+    def __init__(self, fps=30):
         s.SDL_Init(s.SDL_INIT_VIDEO)
 
         self._windowList = dict()
@@ -18,56 +19,76 @@ class SDL2_Dealer(object):
         self._hook = None
         self._framerate = int(1000 / fps)
         self._in_event_loop = False
+        self._running = True
 
-    def add(self, val):
-        self._windowList[val.id] = val
+        # used to lock manually threads when accessing to a shared variable
+        self._windows_lock = Lock()
 
-    def erase(self, val):
-        del self._windowList[val.id]
+        # create thread with the event loop
+        self._create_thread()
 
-    def __del__(self):
-        s.SDL_Quit()
+        # launch the thread with the event loop
+        self.launch_events()
 
-try:
-    from IPython.lib.inputhook import InputHookManager, stdin_ready
+    def _create_thread(self):
+        """
+        Create the thread object to launch the event loop.
+        """
+        if not self._in_event_loop:
+            self._events_thread = Thread(
+                target=self._dealEvents,
+            )
 
-    class SDL2_Deal(SDL2_Dealer):
-        def __init__(self, *args, **kwargs):
+    def launch_events(self):
+        """
+        If the event loop isn't already launched, launch it.
+        """
+        if not self._in_event_loop:
+            self._in_event_loop = True
+            self._events_thread.start()
 
-            super(SDL2_Deal, self).__init__(*args, **kwargs)
-            self._hook = InputHookManager()
-            self.launch_events()
-
-        def launch_events(self):
-            if not self._in_event_loop:
-                self._in_event_loop = True
-
-            def events():
-                self._dealEvents()
-                return 0
-
-            self._hook.set_inputhook(events)
-
-        def _dealEvents(self):
-            while not stdin_ready():
+    def _dealEvents(self):
+        try:
+            # while not stdin_ready():
+            while self._running:
                 start = s.SDL_GetTicks()
 
                 self._ev.update()
 
-                for win in self._windowList.values():
-                    win.events(self._ev)
-                    win.draw()
+                try:
+                    self._windows_lock.acquire(timeout=1)
+                    for win in self._windowList.values():
+                        win.events(self._ev)
+                        win.draw()
+                finally:
+                    self._windows_lock.release()
 
                 stop = s.SDL_GetTicks()
                 duree = (stop - start)
                 if duree < self._framerate:
                     s.SDL_Delay(self._framerate - duree)
+        except Exception as e:
+            print(e)
 
-except:
+    def add(self, val):
+        with self._windows_lock:
+            self._windowList[val.id] = val
 
-    class SDL2_Deal(SDL2_Dealer):
-        def __init__(self, *args, **kwargs):
-            super(SDL2_Deal, self).__init__(*args, **kwargs)
+    def erase(self, val):
+        del self._windowList[val.id]
+
+    def __del__(self):
+        """
+        Clean delete of all variables.
+        """
+
+        print("Coucou")
+        # kill the thread
+        self._running = False
+        self._windows_lock.release()
+
+        # quit SQL
+        s.SDL_Quit()
 
 _ipython_way_sdl2 = SDL2_Deal()
 
@@ -111,15 +132,12 @@ class Figure(object):
     @axes.setter
     def axes(self, value):
 
-        # store the instance for plots
-        self.scene.lines = value
-
         # create shaders if there is one
         self.scene.makeCurrent()
         try:
             value.createShaders(self.scene)
-        except AttributeError:
-            pass
+        except AttributeError as e:
+            print(e)
 
         # add widget created by user
         try:
@@ -128,6 +146,9 @@ class Figure(object):
                 self.addWidget(wid)
         except:
             pass
+
+        # store the instance for plots
+        self.scene.lines = value
 
     def close(self):
         self.scene.close()
